f74559f28751141f78bbcc9217df50484f559b29
Aditya Pawar <aditya.pawar@warriorlife.net>
2021-11-06 13:52:45 -0700

    Created Translations and SwerveDriveKinematics as class variables

--- src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
@@ -8,6 +8,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.team3256.robot.Constants.AutoConstants.*;
 
 public class SwerveDrive extends SubsystemBase {
+    private final Translation2d frontLeft = new Translation2d(kFrontLeft[0], kFrontLeft[1]);
+    private final Translation2d frontRight = new Translation2d(kFrontRight[0], kFrontRight[1]);
+    private final Translation2d backLeft = new Translation2d(kBackLeft[0], kBackLeft[1]);
+    private final Translation2d backRight = new Translation2d(kBackRight[0], kBackRight[1]);
+
+    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
     /**
      * Creates a new ExampleSubsystem.
      */
@@ -24,21 +30,12 @@ public class SwerveDrive extends SubsystemBase {
     * @param omega The angular velocity in rad/s of the robot
      */
     public void drive(double vx, double vy, double omega) {
-        Translation2d frontLeft = new Translation2d(kFrontLeft[0], kFrontLeft[1]);
-        Translation2d frontRight = new Translation2d(kFrontRight[0], kFrontRight[1]);
-        Translation2d backLeft = new Translation2d(kBackLeft[0], kBackLeft[1]);
-        Translation2d backRight = new Translation2d(kBackRight[0], kBackRight[1]);
-
-        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
-
         // takes in vx, vy, and omega (angular velocity)
-        ChassisSpeeds speeds = new ChassisSpeeds(1.0,1.0,1.0);
+        ChassisSpeeds speeds = new ChassisSpeeds(vx, vy, omega);
 
         //converts the chassis speed to an array of module states that give the speed and angle
         SwerveModuleState[] mStates = m_kinematics.toSwerveModuleStates(speeds);
     }
-
-
 
 
 }
