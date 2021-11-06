e1cd8c84388edeec284bdcce1a79ec095987789d
Aditya Pawar <aditya.pawar@warriorlife.net>
2021-11-06 13:31:42 -0700

    Added swerve subsystem
     Added previous SwerveKinematics in the drive method

--- src/main/java/frc/team3256/robot/Constants.java
+++ src/main/java/frc/team3256/robot/Constants.java
@@ -3,6 +3,10 @@ package frc.team3256.robot;
 public final class Constants {
     public static class AutoConstants {
         public static double MIN_SPACE_BETWEEN_POINTS = 0.5;
+        public static final double[] kFrontLeft = {0.5, 0.5};
+        public static final double[] kFrontRight = {0.5,-0.5};
+        public static final double[] kBackLeft = {-0.5,0.5};
+        public static final double[] kBackRight = {-0.5,-0.5};
     }
 
     public static class CANConstants {
--- src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
@@ -4,21 +4,40 @@ import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import frc.team3256.robot.constants.AutoConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.team3256.robot.Constants.AutoConstants.*;
 
-public class SwerveDrive {
-    Translation2d frontLeft = new Translation2d(AutoConstants.kFrontLeft[0], AutoConstants.kFrontLeft[1]);
-    Translation2d frontRight = new Translation2d(AutoConstants.kFrontRight[0], AutoConstants.kFrontRight[1]);
-    Translation2d backLeft = new Translation2d(AutoConstants.kBackLeft[0], AutoConstants.kBackLeft[1]);
-    Translation2d backRight = new Translation2d(AutoConstants.kFrontRight[0], AutoConstants.kFrontRight[1]);
+public class SwerveDrive extends SubsystemBase {
+    /**
+     * Creates a new ExampleSubsystem.
+     */
+    public SwerveDrive() { }
 
-    SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+    /*
+    *
+    *
+    *
+    *
+     */
+    public void drive(double vx, double vy, double omega) {
+        Translation2d frontLeft = new Translation2d(kFrontLeft[0], kFrontLeft[1]);
+        Translation2d frontRight = new Translation2d(kFrontRight[0], kFrontRight[1]);
+        Translation2d backLeft = new Translation2d(kBackLeft[0], kBackLeft[1]);
+        Translation2d backRight = new Translation2d(kBackRight[0], kBackRight[1]);
 
-    // takes in vx, vy, and omega (angular velocity)
-    ChassisSpeeds speeds = new ChassisSpeeds(1.0,1.0,1.0);
+        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
 
-    //converts the chassis speed to an array of module states that give the speed and angle
-    SwerveModuleState[] mStates = m_kinematics.toSwerveModuleStates(speeds);
+        // takes in vx, vy, and omega (angular velocity)
+        ChassisSpeeds speeds = new ChassisSpeeds(1.0,1.0,1.0);
+
+        //converts the chassis speed to an array of module states that give the speed and angle
+        SwerveModuleState[] mStates = m_kinematics.toSwerveModuleStates(speeds);
+    }
+
 
 
 
