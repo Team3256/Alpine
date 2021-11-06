61cfd310289fc0cceefb9de9491c95ee8a536fe8
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2021-11-06 14:30:14 -0700

    Added SwerveDrive Odometry

--- src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
@@ -1,8 +1,12 @@
 package frc.team3256.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.team3256.robot.Constants.AutoConstants.*;
@@ -12,17 +16,37 @@ public class SwerveDrive extends SubsystemBase {
     private final Translation2d frontRight = new Translation2d(kFrontRight[0], kFrontRight[1]);
     private final Translation2d backLeft = new Translation2d(kBackLeft[0], kBackLeft[1]);
     private final Translation2d backRight = new Translation2d(kBackRight[0], kBackRight[1]);
+    private Pose2d m_pose = new Pose2d(5.0, 13.5, new Rotation2d());
+
+    private AnalogGyro m_gyro = new AnalogGyro(0);
 
     private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
+
+    private SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, getGyroHeading(), m_pose);
+
+    private Rotation2d gyroAngle;
+
+    private Rotation2d getGyroHeading() {
+        return gyroAngle;
+    }
+
     /**
      * Creates a new ExampleSubsystem.
      */
-    public SwerveDrive() { }
+    public SwerveDrive() {
+        m_gyro.reset();
+    }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        gyroAngle = Rotation2d.fromDegrees(-m_gyro.getAngle());
+
+        // Update the pose
+        m_pose = m_odometry.update(gyroAngle, m_frontLeftModule.getState(), m_frontRightModule.getState(),
+                m_backLeftModule.getState(), m_backRightModule.getState());
     }
+
     /**
     * Drives a swerve drive drivebase using {@link SwerveDriveKinematics}
     * @param vx The velocity in the x direction relative to the field
