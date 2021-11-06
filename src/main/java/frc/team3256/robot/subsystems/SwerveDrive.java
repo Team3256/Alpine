a4684da8490bb9eb859f5349275ecfc722447708
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2021-11-05 17:57:58 -0700

    added swerve translations, initial chassis speed, and the conversion to get the module states.

--- src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/team3256/robot/subsystems/SwerveDrive.java
@@ -1,5 +1,25 @@
 package frc.team3256.robot.subsystems;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import frc.team3256.robot.constants.AutoConstants;
+
 public class SwerveDrive {
-    //to see folder
+    Translation2d frontLeft = new Translation2d(AutoConstants.kFrontLeft[0], AutoConstants.kFrontLeft[1]);
+    Translation2d frontRight = new Translation2d(AutoConstants.kFrontRight[0], AutoConstants.kFrontRight[1]);
+    Translation2d backLeft = new Translation2d(AutoConstants.kBackLeft[0], AutoConstants.kBackLeft[1]);
+    Translation2d backRight = new Translation2d(AutoConstants.kFrontRight[0], AutoConstants.kFrontRight[1]);
+
+    SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeft, frontRight, backLeft, backRight);
+
+    // takes in vx, vy, and omega (angular velocity)
+    ChassisSpeeds speeds = new ChassisSpeeds(1.0,1.0,1.0);
+
+    //converts the chassis speed to an array of module states that give the speed and angle
+    SwerveModuleState[] mStates = m_kinematics.toSwerveModuleStates(speeds);
+
+
+
 }
